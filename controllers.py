006f65720e08e531886757cb78dbd218e09521d8
Dario Ferreira <fcdario@gmail.com>
2023-12-13 04:52:02 +0100

    Arreglar ruta logica para las imagenes.

--- controllers.py
+++ controllers.py
@@ -5,8 +5,10 @@ from app import app,ma
 import time
 import os
 
-# Establecer la carpeta pública del servidor que aloja las imágenes de los ingredientes
+# Establecer la carpeta pública del servidor que aloja las imágenes de los productos
 app.config['FOLDER_IMG_PRODUCTOS'] = 'public/img/productos'
+# Establecer la ruta lógica del servidor que aloja las imágenes de los productos
+app.config['PATH_IMG_PRODUCTOS'] = 'api/productos/img'
 
 class ProductoSchema(ma.Schema):
     class Meta:
@@ -15,24 +17,24 @@ class ProductoSchema(ma.Schema):
 producto_schema=ProductoSchema()            # El objeto producto_schema es para traer un producto
 productos_schema=ProductoSchema(many=True)  # El objeto productos_schema es para traer multiples registros de producto
 # crea los endpoint o rutas (json)
-@app.route('/productos',methods=['GET'])
+@app.route('/api/productos',methods=['GET'])
 def get_Productos():
     all_productos=Producto.query.all()         # el metodo query.all() lo hereda de db.Model
     result=productos_schema.dump(all_productos)  # el metodo dump() lo hereda de ma.schema y
                                                  # trae todos los registros de la tabla
     return jsonify(result)                       # retorna un JSON de todos los registros de la tabla
 
-@app.route('/productos/<id>',methods=['GET'])
+@app.route('/api/productos/<id>',methods=['GET'])
 def get_producto(id):
     producto=Producto.query.get(id)
     return producto_schema.jsonify(producto)   # retorna el JSON de un producto recibido como parametro
 
-@app.route('/api/protuctos/img/<filename>',methods=['GET'])
+@app.route(f"/{app.config['PATH_IMG_PRODUCTOS']}/<filename>",methods=['GET'])
 def get_img_producto_by_id(filename):
     return send_from_directory(app.config['FOLDER_IMG_PRODUCTOS'], filename)
 
 
-@app.route('/productos', methods=['POST']) # crea ruta o endpoint
+@app.route('/api/productos', methods=['POST']) # crea ruta o endpoint
 def create_producto():
     nombre=request.form['nombre']
     precio=request.form['precio']
@@ -49,7 +51,7 @@ def create_producto():
     foto.save(os.path.join(app.config['FOLDER_IMG_PRODUCTOS'], nombre_imagen))
 
     #foto.filename = nombre_imagen
-    url_foto = f"/{app.config['FOLDER_IMG_PRODUCTOS']}/{nombre_imagen}"
+    url_foto = f"/{app.config['PATH_IMG_PRODUCTOS']}/{nombre_imagen}"
 
     new_producto=Producto(nombre,precio,stock,url_foto,descripcion)
     db.session.add(new_producto)
@@ -57,7 +59,7 @@ def create_producto():
     return producto_schema.jsonify(new_producto)
 
 
-@app.route('/productos/<id>' ,methods=['PUT'])
+@app.route('/api/productos/<id>' ,methods=['PUT'])
 def update_producto(id):
     # Obtengo el producto a actualizar
     producto=Producto.query.get(id)
@@ -76,7 +78,7 @@ def update_producto(id):
     foto.filename = nombre_imagen
     foto.save(os.path.join(app.config['FOLDER_IMG_PRODUCTOS'], nombre_imagen))
 
-    url_foto = f"/{app.config['FOLDER_IMG_PRODUCTOS']}/{nombre_imagen}"
+    url_foto = f"/{app.config['PATH_IMG_PRODUCTOS']}/{nombre_imagen}"
 
     producto.nombre=nombre
     producto.precio=precio
@@ -88,7 +90,7 @@ def update_producto(id):
     return producto_schema.jsonify(producto)
 
 
-@app.route('/productos/<id>',methods=['DELETE'])
+@app.route('/api/productos/<id>',methods=['DELETE'])
 def delete_producto(id):
     producto=Producto.query.get(id)
     db.session.delete(producto)
